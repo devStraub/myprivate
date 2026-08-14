1eba0b942e162e32773dfcec8db2504a1c242713
michel.pech <michelhphz@hotmail.com>
2026-08-14 14:50:42 -0300

    fix: corrige aviso de compatibilidade RedM

--- resources/[fipas]/fipas-core/fxmanifest.lua
+++ resources/[fipas]/fipas-core/fxmanifest.lua
@@ -1,6 +1,6 @@
 fx_version 'cerulean'
 game 'rdr3'
-rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources will become incompatible once RedM ships.'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 author 'Fipas Roleplay'
 description 'Camada central de configuracao e extensoes do Fipas Roleplay'
